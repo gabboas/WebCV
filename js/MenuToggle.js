b931b963c3508cbd95d3fefc8bb2276e76bd00fc
gabboas <gmaia@outlook.ie>
2024-02-13 00:20:41 +0000

    MenuToggle working 100%, layout decided and no bugs.

--- js/MenuToggle.js
+++ js/MenuToggle.js
@@ -1,11 +1,15 @@
 let menu = document.getElementById("span__toggle");
-let menu_item = document.getElementById("nav__toggle");
-let div = document.getElementById("expansiondiv__container");
+let menu_items = document.getElementById("nav__toggle");
+let title = document.getElementById("title__h2");
 
 menu.addEventListener("mouseenter", function () {
-  menu_item.classList.add("aparece");
+  menu_items.classList.add("showing");
 });
 
-menu_item.addEventListener("mouseleave", function () {
-  menu_item.classList.remove("aparece");
+menu_items.addEventListener("mouseleave", function () {
+  menu_items.classList.remove("showing");
+});
+
+title.addEventListener("mouseenter", function() {
+  menu_items.classList.remove("showing");
 });
